228d65fea2b4fc226ff19c8c45c8d073eb062dcb
hasan.binboga@sirenbilisim.com.tr <hasanbinboga@gmail.com>
2022-11-12 00:53:07 +0300

    Create DateExtensions.cs

---
+++ net-framework/NetFrame/Common/NetFrame.Common.Extension/DateExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace NetFrame.Common.Extension
+{
+    /// <summary>
+    /// Date extensions class
+    /// </summary>
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Method for getting first day of month
+        /// </summary>
+        /// <param name="date">Date</param>
+        /// <returns>First Day of month</returns>
+		public static DateTime FirstDayOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+        /// <summary>
+        /// Method for getting last day of month.
+        /// </summary>
+        /// <param name="date">Date</param>
+        /// <returns>Last Day Of Month</returns>
+		public static DateTime LastDayOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1).AddMonths(1).AddDays(-1);
+        }
+    }
+}
