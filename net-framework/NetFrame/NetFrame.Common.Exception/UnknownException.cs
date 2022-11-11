e4378677558f882533a668eb9d6b463c2b95fe74
hasan.binboga@sirenbilisim.com.tr <hasan.binboga@sirenbilisim.com.tr>
2022-11-11 22:20:45 +0300

    Create UnknownException.cs

---
+++ net-framework/NetFrame/NetFrame.Common.Exception/UnknownException.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Serialization;
+
+namespace NetFrame.Common.Exception
+{
+    /// <summary>
+    /// Unknown exception class
+    /// </summary>
+    public class UnknownException : BaseException
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UnknownException()
+        {
+            PersistException(this);
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Unknown Exception message information</param>
+        public UnknownException(string message) : base(message)
+        {
+            PersistException(this);
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Unknown Exception message information</param>
+        /// <param name="exception">Exception</param>
+        public UnknownException(string message, BaseException exception) : base(message, exception)
+        {
+            PersistException(this);
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="serializationInfo">Unknown Exception serialization information</param>
+        /// <param name="context">Unknown Exception stream context information</param>
+        public UnknownException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+            PersistException(this);
+        }
+    }
+}
